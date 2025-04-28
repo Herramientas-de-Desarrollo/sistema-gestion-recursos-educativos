d3b2b0a0d4cceccd1e864758a3a387920ce8347b
Fernando Raul <prueba@correo.com>
2025-04-28 14:35:38 -0500

    Core: Test

--- src/main/java/com/herramientas/desarrollo/sistema_gestion_recursos_educativos/SistemaGestionRecursosEducativosApplication.java
+++ src/main/java/com/herramientas/desarrollo/sistema_gestion_recursos_educativos/SistemaGestionRecursosEducativosApplication.java
@@ -9,5 +9,5 @@ public class SistemaGestionRecursosEducativosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaGestionRecursosEducativosApplication.class, args);
 	}
-
+//Inicio del sistema
 }
