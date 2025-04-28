c41849a5d86502236b33bcf446ee7e9f59457114
Fernando Raul <prueba@correo.com>
2025-04-28 14:26:39 -0500

    Initial commit

---
+++ src/main/java/com/herramientas/desarrollo/sistema_gestion_recursos_educativos/SistemaGestionRecursosEducativosApplication.java
@@ -0,0 +1,13 @@
+package com.herramientas.desarrollo.sistema_gestion_recursos_educativos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionRecursosEducativosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionRecursosEducativosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herramientas/desarrollo/sistema_gestion_recursos_educativos/SistemaGestionRecursosEducativosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herramientas.desarrollo.sistema_gestion_recursos_educativos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionRecursosEducativosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
