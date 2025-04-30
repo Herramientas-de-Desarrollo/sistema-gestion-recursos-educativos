2345f2a6730b2da8406fe941da894519ccf474f8
BuildTools <unconfigured@null.spigotmc.org>
2025-04-30 18:23:19 -0500

    encriptacion funcional

--- src/main/java/com/herramientas/desarrollo/sistema_gestion_recursos_educativos/dto/RegisterDTO.java
+++ src/main/java/com/herramientas/desarrollo/sistema_gestion_recursos_educativos/dto/RegisterDTO.java
@@ -1,5 +1,6 @@
 package com.herramientas.desarrollo.sistema_gestion_recursos_educativos.dto;
 
+import com.herramientas.desarrollo.sistema_gestion_recursos_educativos.model.Rol;
 import lombok.Data;
 
 @Data
@@ -9,4 +10,10 @@ public class RegisterDTO {
     private String correo;
     private String clave;
     private String rol; // luego se convierte a Enum
+
+    public Rol getRolEnum() {
+        return Rol.valueOf(this.rol.toUpperCase());
+    }
 }
+
+
