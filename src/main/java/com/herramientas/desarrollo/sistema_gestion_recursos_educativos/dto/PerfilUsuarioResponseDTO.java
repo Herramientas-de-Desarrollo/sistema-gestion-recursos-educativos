0cbd2741cf4af2377a457311ed9c0216cfc564f5
Fernando Raul <prueba@correo.com>
2025-05-28 12:01:47 -0500

    Core: Agregar clases dto de PerfilUsuario

---
+++ src/main/java/com/herramientas/desarrollo/sistema_gestion_recursos_educativos/dto/PerfilUsuarioResponseDTO.java
@@ -0,0 +1,10 @@
+package com.herramientas.desarrollo.sistema_gestion_recursos_educativos.dto;
+
+import lombok.Data;
+
+@Data
+public class PerfilUsuarioResponseDTO {
+    private String biografia;
+    private String intereses;
+    private String area;
+}
