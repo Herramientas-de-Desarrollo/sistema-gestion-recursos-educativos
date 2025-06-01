6db5eca1f7734540ee69ceb34615d38a8f0e60e4
Fernando Raul <prueba@correo.com>
2025-06-01 18:28:33 -0500

    Core: Añadir clase repository de recursos educativos

---
+++ src/main/java/com/herramientas/desarrollo/sistema_gestion_recursos_educativos/repository/RecursoEducativoRepository.java
@@ -0,0 +1,6 @@
+package com.herramientas.desarrollo.sistema_gestion_recursos_educativos.repository;
+
+import com.herramientas.desarrollo.sistema_gestion_recursos_educativos.model.RecursoEducativo;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface RecursoEducativoRepository extends JpaRepository<RecursoEducativo, Long>{
+}
