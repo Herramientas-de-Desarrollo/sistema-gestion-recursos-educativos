e1d5374e4d7917a448c527c87272470bf2af3bf6
Fernando Raul <prueba@correo.com>
2025-06-01 18:28:56 -0500

    Core: Añadir clase DTO de recursos educativos

---
+++ src/main/java/com/herramientas/desarrollo/sistema_gestion_recursos_educativos/dto/RecursoEducativoRespuestaDTO.java
@@ -0,0 +1,16 @@
+package com.herramientas.desarrollo.sistema_gestion_recursos_educativos.dto;
+
+import lombok.Data;
+
+@Data
+public class RecursoEducativoRespuestaDTO {
+    private Long id;
+    private String titulo;
+    private String descripcion;
+    private String tipo;
+    private String url;
+    private String nombreArchivo;
+
+    private Long autorId;
+    private String autorNombre;
+}
